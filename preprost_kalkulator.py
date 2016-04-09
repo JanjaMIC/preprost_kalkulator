b4574898bdeb57d4f2af7d733cef3401c8838ed7
Janja <janjakrizman@gmail.com>
2016-04-09 13:18:13 +0200

    first commit

---
+++ preprost_kalkulator.py
@@ -0,0 +1,19 @@
+st_1 = float(raw_input("Izberi prvo stevilo: "))
+st_2 = float(raw_input("Izberi drugo stevilo: "))
+
+
+
+operacija = raw_input("Izberi operacijo: deljenje, mnozenje, sestevanje, odstevanje:").lower()
+
+print ("Racunalnik bo sedaj izvedel operacijo, ki ste jo izbrali za izbrani stevilki. Pricenja se... " + operacija + "!")
+
+if operacija == "deljenje":
+    print ("Rezultat je naslednji", st_1 / st_2)
+elif operacija == "mnozenje":
+    print ("Rezultat je naslednji:", st_1 * st_2)
+elif operacija == "sestevanje":
+    print ("Rezultat je naslednji: ", st_1 + st_2)
+elif operacija == "odstevanje":
+    print ("Rezultat je naslednji: ", st_1 - st_2)
+else:
+    print ("Napaka.Morda ste se zatipkali")
