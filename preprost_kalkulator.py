0a9a74f0a86f8d9ab700eea6fede0fda66acde0f
Janja <janjakrizman@gmail.com>
2016-04-10 09:47:35 +0200

    s funkcijo

--- preprost_kalkulator.py
+++ preprost_kalkulator.py
@@ -1,21 +1,22 @@
 st_1 = float(raw_input("Izberi prvo stevilo: "))
 st_2 = float(raw_input("Izberi drugo stevilo: "))
 
-
 operacija = (raw_input("Izberi operacijo: deljenje, mnozenje, sestevanje, odstevanje:").lower())
 
 print "Racunalnik bo sedaj izvedel operacijo, ki ste jo izbrali za izbrani stevilki. Pricenja se... " + operacija + "!"
 
-while True:
+def racunalo(st_1, st_2):
     if operacija == "deljenje":
         print "Rezultat je naslednji", st_1 / st_2
-        break
+
     elif operacija == "mnozenje":
         print "Rezultat je naslednji:", st_1 * st_2
-        break
+
     elif operacija == "sestevanje":
         print "Rezultat je naslednji: ", st_1 + st_2
-        break
+
     elif operacija == "odstevanje":
         print "Rezultat je naslednji: ", st_1 - st_2
-        break
+        return racunalo(st_1, st_2)
+
+print racunalo(st_1, st_2)
